1975b1a96ccfa0b9cf422612157199a376544d2f
maximo-wiki-user <66974934+maximo-wiki-user@users.noreply.github.com>
2021-03-20 11:46:44 +0000

    Create mxc_groovy_baby.groovy
    initial version

---
+++ mxc_groovy_baby.groovy
@@ -0,0 +1,53 @@
+# Groovy automation script for IBM Maximo to retrieve
+# OS, memory and garbage collection info  
+
+# See blog post by Rana Ahmed for steps to add Groovy language support to IBM Maximo
+# https://maximomize.wordpress.com/2020/01/27/add-groovy-support-to-ibm-maximo-icd/
+ 
+
+import java.lang.management.*
+
+def os = ManagementFactory.operatingSystemMXBean 
+println """OPERATING SYSTEM: 
+\tOS architecture = $os.arch 
+\tOS name = $os.name 
+\tOS version = $os.version 
+\tOS processors = $os.availableProcessors 
+""" 
+ 
+def rt = ManagementFactory.runtimeMXBean 
+println """RUNTIME: 
+   \tRuntime name = $rt.name 
+   \tRuntime spec name = $rt.specName 
+   \tRuntime vendor = $rt.specVendor 
+   \tRuntime spec version = $rt.specVersion 
+   \tRuntime management spec version = $rt.managementSpecVersion 
+   """ 
+
+def mem = ManagementFactory.memoryMXBean 
+def heapUsage = mem.heapMemoryUsage 
+def nonHeapUsage = mem.nonHeapMemoryUsage 
+
+println """MEMORY: 
+   HEAP STORAGE: 
+      \tMemory committed = $heapUsage.committed 
+      \tMemory init = $heapUsage.init 
+      \tMemory max = $heapUsage.max 
+      \tMemory used = $heapUsage.used NON-HEAP STORAGE: 
+      \tNon-heap memory committed = $nonHeapUsage.committed 
+      \tNon-heap memory init = $nonHeapUsage.init 
+      \tNon-heap memory max = $nonHeapUsage.max 
+      \tNon-heap memory used = $nonHeapUsage.used 
+   """
+  
+println "GARBAGE COLLECTION:" 
+ManagementFactory.garbageCollectorMXBeans.each { gc ->
+   println "\tname = $gc.name"
+   println "\t\tcollection count = $gc.collectionCount"
+   println "\t\tcollection time = $gc.collectionTime"
+   String[] mpoolNames =   gc.memoryPoolNames
+	
+   mpoolNames.each { 
+      mpoolName -> println "\t\tmpool name = $mpoolName"
+   } 
+}
